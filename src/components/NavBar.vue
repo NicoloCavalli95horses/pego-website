f84c3c551ec06298a47964d09611667231b37f5c
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2023-05-26 17:15:36 +0200

    improved mobile menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,25 +16,29 @@
       <Icon icon="fa-solid fa-bars" class="svg-24" />
     </div>
   </nav>
-
-  <Modal
-    v-if="show.mobile_menu"
-    :full_size="true"
-    :close_btn="true"
-    @closed="show.mobile_menu = false"
-    >
-    <div class="mobile-nav-options">
-      <div @click="show.mobile_menu = false">
-        <a href="#about"><h3>Chi siamo</h3></a>
-     </div>
-      <div @click="show.mobile_menu = false">
-        <a href="#where"><h3>Dove operiamo</h3></a>
-     </div>
-      <div @click="show.mobile_menu = false">
-        <a href="#faq"><h3>Domande frequenti</h3></a>
-     </div>
+  
+  <div :class="{ 'active' : show.mobile_menu }" class="mobile-menu">
+    <div class="close-menu bottom-12">
+      <Icon icon="fa-solid fa-close" class="svg-24" @click="show.mobile_menu = false" />
     </div>
-  </Modal>
+    <div class="nav-options">
+      <a class="option" href="#about" @click="show.mobile_menu = false">
+        <h4>Chi siamo</h4>
+        <Icon icon="fa-solid fa-solid fa-chevron-right" class="svg-24" />
+      </a>
+      <a class="option" href="#where" @click="show.mobile_menu = false">
+        <h4>Dove operiamo</h4>
+        <Icon icon="fa-solid fa-solid fa-chevron-right" class="svg-24" />
+      </a>
+      <a class="option" href="#faq" @click="show.mobile_menu = false">
+        <h4>Domande frequenti</h4>
+        <Icon icon="fa-solid fa-solid fa-chevron-right" class="svg-24" />
+      </a>
+      <div class="w-100 flex-center top-48">
+        <Btn text="contattaci" :def="true" @click="() => { show.mobile_menu = false; $router.push('/contact'); }" />
+      </div>
+    </div>
+ </div>
 
 </template>
 
@@ -47,8 +51,9 @@ import { RouterLink } from "vue-router";
 import { getViewport } from "../utils/screen_size.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+
 import Logo from "./Logo.vue";
-import Modal from "./Modal.vue";
+import Btn from "./Btn.vue";
 
 //==================================
 // Consts
@@ -95,12 +100,49 @@ nav {
    }
   }
 }
-.mobile-nav-options {
+
+.mobile-menu {
+  position: fixed;
   width: 100%;
-  height: 40vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
+  height: 100vh;
+  background-color: var(--footer-bg);
+  top: 0;
+  left: 0;
+  transform: translate(100%, 0);
+  transition-duration: var(--transition-slow);
+  opacity: 0;
+  z-index: -1;
+  &.active {
+    transform: translate(0, 0);
+    transition-duration: var(--transition-slow);
+    z-index: 2;
+    opacity: 1;
+  }
+  .close-menu {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 2rem;
+    svg {
+      cursor: pointer;
+    }
+  }
+  .nav-options {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    padding: 0 2.3rem;
+    cursor: pointer;
+    transition-duration: var(--transition-medium);
+    .option {
+      width: 100%;
+      padding: 2.3rem 0;
+      border-bottom: 0.2rem solid var(--primary);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
 }
 </style>
